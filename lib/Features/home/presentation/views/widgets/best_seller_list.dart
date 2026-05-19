d75ebc05a91c50266b17ebe597a1faebe53aca27
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-05-19 04:02:12 +0300

    best seller  item part 3

--- lib/Features/home/presentation/views/widgets/best_seller_list.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_list.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constansts.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -25,6 +26,7 @@ class BestSellerListView extends StatelessWidget {
           ),
           SizedBox(width: 30),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * .5,
@@ -32,8 +34,21 @@ class BestSellerListView extends StatelessWidget {
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
                   'Harry Potter and the Goblet of Fire ',
-                  style: Styles.textStyle20,
+                  style: Styles.textStyle20.copyWith(fontFamily: kGTSectraFine),
                 ),
+              ),
+              SizedBox(height: 3),
+              Text('j.k. Rowling', style: Styles.textStyle14),
+              SizedBox(height: 3),
+              Row(
+                children: [
+                  Text(
+                    '19.99 €',
+                    style: Styles.textStyle20.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
