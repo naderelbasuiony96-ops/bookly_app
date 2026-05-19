b3dd9518d45cddd6583afa43f360bf193e03465c
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-05-19 18:24:21 +0300

    create book details view

---
+++ lib/Features/home/presentation/views/book_view_details.dart
@@ -0,0 +1,11 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/book_details_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: BookDetailsBody());
+  }
+}
