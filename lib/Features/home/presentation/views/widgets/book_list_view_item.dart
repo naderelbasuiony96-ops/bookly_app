21d89659c9300ecd2aa9e279906a9b91b56d6a95
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-05-25 10:30:10 +0300

    done search view

--- lib/Features/home/presentation/views/widgets/book_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/book_list_view_item.dart
@@ -6,8 +6,8 @@ import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class BestSellerItems extends StatelessWidget {
-  const BestSellerItems({super.key});
+class BookListViewItems extends StatelessWidget {
+  const BookListViewItems({super.key});
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/Features/search/presentation/views/widgets/search_result_list.dart';
 import 'package:bookly_app/Features/search/presentation/views/widgets/search_text_field.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -8,7 +10,16 @@ class SearchViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(children: [SearchTextField()]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SearchTextField(),
+          const SizedBox(height: 16),
+          Text('Search Result', style: Styles.textStyle18),
+          const SizedBox(height: 16),
+          Expanded(child: SearchResultListView()),
+        ],
+      ),
     );
   }
 }
