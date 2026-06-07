b37a62992d2504a68141eebd817d54db6fa8cba8
nader amr elbasuiony <naderelbasuiony96@gmail.com>
2026-06-07 14:12:41 +0300

    create best seller books cubit method

--- lib/features/home/presentation/manger/best_seller_books_cubit/best_seller_books_cubit.dart
+++ lib/features/home/presentation/manger/best_seller_books_cubit/best_seller_books_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bookly_app/features/home/data/models/bookly_model/bookly_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'best_seller_books_state.dart';
 
 class BestSellerBooksCubit extends Cubit<BestSellerBooksState> {
-  BestSellerBooksCubit() : super(BestSellerBooksInitial());
+  BestSellerBooksCubit(this.homeRepo) : super(BestSellerBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(BestSellerBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold(
+      (failure) {
+        emit(BestSellerBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(BestSellerBooksSuccess(books));
+      },
+    );
+  }
 }
